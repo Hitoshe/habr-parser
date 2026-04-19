a28a633ece6ffa2da8c8a0b59581da50698b34cd
Hitoshe <fire.dragon.1993@mail.ru>
2026-04-20 00:09:58 +0300

    Initial commit

---
+++ src/main/java/com/dev/habr_parser/HabrParserApplication.java
@@ -0,0 +1,13 @@
+package com.dev.habr_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabrParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabrParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/habr_parser/HabrParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.habr_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabrParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

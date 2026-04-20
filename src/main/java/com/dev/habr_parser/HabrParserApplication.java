241976add035fb60f84c9889ff7341b2dc68b3dd
Hitoshe <fire.dragon.1993@mail.ru>
2026-04-20 20:18:24 +0300

    fix

--- src/main/java/com/dev/habr_parser/HabrParserApplication.java
+++ src/main/java/com/dev/habr_parser/HabrParserApplication.java
@@ -1,5 +1,4 @@
 package com.dev.habr_parser;
-
 import com.fasterxml.jackson.databind.ObjectMapper; // импорт
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
